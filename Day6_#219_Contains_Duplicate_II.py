4d9c2c0d573e10d1d0afb0cab5f29aa596f33d84
doduc1709 <minhducdt0101@gmail.com>
2026-09-10 21:54:32 +0700

    Bài tập vận dụng ngẫu nhiên

---
+++ Day6_#219_Contains_Duplicate_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums, k):
+        seen = {}   # {giá_trị: chỉ_số_gần_nhất}
+        
+        for i in range(len(nums)):
+            if nums[i] in seen:
+                # đã gặp giá trị này trước đó rồi
+                if i - seen[nums[i]] <= k :   # điều kiện khoảng cách <= k
+                    return True
+            
+            seen[nums[i]] = i   # cập nhật vị trí gần nhất của giá trị này
+        
+        return False
