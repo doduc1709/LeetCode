14096498478e811b787d23e5a42d8ec20f45f977
doduc1709 <minhducdt0101@gmail.com>
2026-09-12 22:46:44 +0700

    Học kỹ thuật Two Pointer

--- Day3_#167_TwoSum2.py
+++ Day3_#167_TwoSum2.py
@@ -2,10 +2,9 @@ class Solution:
     def twoSum(self, numbers, target):
         left = 0
         right = len(numbers) - 1
-        
+
         while left < right:
             current_sum = numbers[left] + numbers[right]
-            
             if current_sum == target:
                 return [left + 1, right + 1]   # trả về chỉ số dạng 1-based
             elif current_sum < target:
