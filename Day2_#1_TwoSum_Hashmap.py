09197590393b67114f530158179caf9d8ea13823
doduc1709 <minhducdt0101@gmail.com>
2026-09-12 22:45:05 +0700

    solve: twoSum with Hashmap

--- Day2_#1_TwoSum_Hashmap.py
+++ Day2_#1_TwoSum_Hashmap.py
@@ -4,13 +4,9 @@ target = 9
 class Solution:
     def twoSum(self, nums, target):
         seen = {}  # dict lưu {giá_trị: chỉ_số} đã duyệt qua
-
         for i in range(len(nums)):
             complement = target - nums[i]   # số còn thiếu cần tìm
-            
             if complement in seen:
                 return [seen[complement], i]   # trả về chỉ số của complement và chỉ số i
-            
             seen[nums[i]] = i   # lưu giá trị hiện tại vào dict
-
 print(Solution().twoSum(nums, target))
