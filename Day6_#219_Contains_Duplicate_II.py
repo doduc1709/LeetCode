67b3d0416845c97934acba2ca5f88020d19c2f54
doduc1709 <minhducdt0101@gmail.com>
2026-09-12 22:49:17 +0700

    Làm bài tập ngãu nhiên áp dụng 1 trong 4 kỹ thuật trên

--- Day6_#219_Contains_Duplicate_II.py
+++ Day6_#219_Contains_Duplicate_II.py
@@ -7,7 +7,5 @@ class Solution:
                 # đã gặp giá trị này trước đó rồi
                 if i - seen[nums[i]] <= k :   # điều kiện khoảng cách <= k
                     return True
-            
             seen[nums[i]] = i   # cập nhật vị trí gần nhất của giá trị này
-        
         return False
