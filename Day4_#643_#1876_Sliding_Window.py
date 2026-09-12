10928d49fcade97b3f35f26b39a775e9b8ba1396
doduc1709 <minhducdt0101@gmail.com>
2026-09-12 22:47:40 +0700

    Học kỹ thuật Sliding Window

--- Day4_#643_#1876_Sliding_Window.py
+++ Day4_#643_#1876_Sliding_Window.py
@@ -29,8 +29,7 @@ class Solution:
         count = 0
         
         for i in range(len(s) - 2):
-            window = s[i : i + 3]      # lấy đoạn con 3 ký tự bắt đầu từ i
-            
+            window = s[i : i + 3]      # lấy đoạn con 3 ký tự bắt đầu từ i 
             if len(set(window)) == 3:
                 count += 1
         return count
